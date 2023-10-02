7823947603476420a813ccf5fbe43be7730868ed
Hamza Bashir <hamzabashir218@gmail.com>
2023-10-02 16:06:56 +0500

    Api

---
+++ api.js
@@ -0,0 +1,9 @@
+const { default: axios } = require("axios");
+
+axios.get("https://www.google.com").then((response) => {
+    console.log(response.data);
+})
+
+axios.get("https://www.google.com").then((response) => {
+    console.log(response.data);
+})
